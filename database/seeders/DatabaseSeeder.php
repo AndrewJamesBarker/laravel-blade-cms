3d8460a81fa144ecf32c81050fc2da6947139af9
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-03-29 16:40:23 -0400

    create entries crud

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Type;
 use App\Models\Project;
+use App\Models\Entry;
+use App\Models\Education;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,14 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         User::truncate();
         Type::truncate();
         Project::truncate();
+        Entry::truncate();
+        Education::truncate();
         
         User::factory()->count(2)->create();
         Type::factory()->count(3)->create();
         Project::factory()->count(4)->create();
-            
+        Entry::factory()->count(4)->create();
+        Education::factory()->count(4)->create();
     }
 }
